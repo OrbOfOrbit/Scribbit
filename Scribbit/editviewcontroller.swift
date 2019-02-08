7e529a084feddab9e503ed3df53ad6f4d392d2d8
Alexander Romine <s013386@lm-20s013386.lmsd.org>
2019-02-08 13:03:16 -0500

    updated to a more recent version of my table view controller

--- Scribbit/editviewcontroller.swift
+++ Scribbit/editviewcontroller.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class editViewController: UITableViewController  {
     //@IBOutlet var tableview: UITableView!
+   
     @IBOutlet var tableview: UITableView!
     let basicarray = [item(newcontent: "s", newdone: false, newsubitems: [item]()), item(newcontent: "b", newdone: false, newsubitems: [item]())]
     var basicitem : item = item(newcontent: "", newdone: true, newsubitems: [item]())
@@ -32,34 +33,44 @@ class editViewController: UITableViewController  {
                 self.basiclist.title = textField.text!
             }))
             self.present(alert, animated: true, completion: nil)
-            tableView.reloadData()
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         }
-            
         else if indexPath.row == basiclist.items.count+1{
-            print("test case alpha")
-        }/*
+            let alert = UIAlertController(title: "new item", message: "", preferredStyle: .alert)
+            alert.addTextField { (textField) in
+                textField.text = "item name"
+            }
+            alert.addAction(UIAlertAction(title: "add", style: .default, handler: { [weak alert] (_) in
+                self.basiclist.items.append(item(newcontent: (alert?.textFields![0].text)!, newdone: false, newsubitems: [item]()))
+                }
+            ))
+            self.present(alert, animated: true, completion: nil)
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
         else{
             let alert = UIAlertController(title: "item", message: "", preferredStyle: .alert)
             alert.addTextField { (textField) in
                 textField.text = self.basiclist.items[indexPath.row-1].content
             }
-            alert.addAction(UIAlertAction(title: "finished", style: .default, handler: { [weak alert] (_) in
+            alert.addAction(UIAlertAction(title: "done", style: .default, handler: { [weak alert] (_) in
                 let textField = alert!.textFields![0]
                 self.basiclist.items[indexPath.row-1].content = textField.text!
-                self.basiclist.items[indexPath.row-1].done=true
             }))
-            alert.addAction(UIAlertAction(title: "unfinished", style: .default, handler: { [weak alert] (_) in
-                let textField = alert!.textFields![0]
-                self.basiclist.items[indexPath.row-1].content = textField.text!
-                self.basiclist.items[indexPath.row-1].done=false
-            }))
+            
             self.present(alert, animated: true, completion: nil)
             tableView.reloadData()
-        }*/
-        tableView.reloadData()
+        }
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-         return basiclist.items.count+2
+        return basiclist.items.count+2
         
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -67,16 +78,18 @@ class editViewController: UITableViewController  {
         if indexPath.row == 0{
             cell?.textLabel?.text = String("Title:\(basiclist.title)")
         }
-       else if indexPath.row == basiclist.items.count+1{
+        else if indexPath.row == basiclist.items.count+1{
             cell?.textLabel?.text = "add more"
         }
         else{
             cell?.textLabel?.text = basiclist.items[indexPath.row-1].content
             if basiclist.items[indexPath.row-1].done == true{
                 cell?.textLabel?.text?.append("[✓]")
+                cell?.setNeedsDisplay()
             }
             else{
                 cell?.textLabel?.text?.append("[X]")
+                cell?.setNeedsDisplay()
             }
         }
         
@@ -86,15 +99,21 @@ class editViewController: UITableViewController  {
     {
         // 1
         if indexPath.row != 0 && indexPath.row != basiclist.items.count+1{
-        let doneaction = UITableViewRowAction(style: UITableViewRowAction.Style.default, title: "done" , handler: { (action:UITableViewRowAction, indexPath: IndexPath) -> Void in
-            self.basiclist.items[indexPath.row-1].done = true
-            
-        })
-        // 3
-        let notdoneaction = UITableViewRowAction(style: UITableViewRowAction.Style.default, title: "not done" , handler: { (action:UITableViewRowAction, indexPath:IndexPath) -> Void in
-            self.basiclist.items[indexPath.row-1].done = false
-        })
-        // 5
+            let doneaction = UITableViewRowAction(style: UITableViewRowAction.Style.default, title: "done" , handler: { (action:UITableViewRowAction, indexPath: IndexPath) -> Void in
+                self.basiclist.items[indexPath.row-1].done = true
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+                
+            })
+            // 3
+            let notdoneaction = UITableViewRowAction(style: UITableViewRowAction.Style.default, title: "not done" , handler: { (action:UITableViewRowAction, indexPath:IndexPath) -> Void in
+                self.basiclist.items[indexPath.row-1].done = false
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            })
+            // 5
             return [doneaction,notdoneaction]}
         return [UITableViewRowAction]()
     }
