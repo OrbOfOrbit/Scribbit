e582166e3335e2d9776a236713dcd21d71e64f32
Alexander Romine (student LM) <s013386@lm-20s013386.lmsd.org>
2019-02-08 10:55:34 -0500

    fixed some  errors regarding list

--- Scribbit/list.swift
+++ Scribbit/list.swift
@@ -2,21 +2,20 @@
 //  list.swift
 //  Scribbit
 //
-//  Created by Alexander Romine (student LM) on 1/17/19.
+//  Created by Alexander Romine (student LM) on 1/30/19.
 //  Copyright © 2019 Oron Barash (student LM). All rights reserved.
 //
 
 import Foundation
-class list{
-   public var done:Bool
-   public var text:String
-    public var title:String
-   public var sublist:list?
-    init(newtitle:String, newsublist:list, newtext:String ,newdone:Bool ) {
-        done = newdone
-        text = newtext
-        sublist = newsublist
-        title = newtitle
+class list {
+    var complete:Bool
+    var created:Date
+    var items:[item]
+    var title:String
+    init(newitems:[item], newtitle:String, newcreated:Date) {
+        created=newcreated
+        complete=false
+        items=newitems
+        title=newtitle
     }
-    
 }
