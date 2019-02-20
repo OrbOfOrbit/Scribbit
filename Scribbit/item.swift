3aba206df16eed9899c90171c47f4a9a21b6eb5d
Justin Cannan (student LM) <s016298@students.lmsd.org>
2019-02-20 18:44:56 -0500

    merged list and master

--- Scribbit/item.swift
+++ Scribbit/item.swift
@@ -5,12 +5,11 @@
 //  Created by Alexander Romine (student LM) on 1/30/19.
 //  Copyright © 2019 Oron Barash (student LM). All rights reserved.
 //
-
 import Foundation
 class item{
-     var done:Bool
-     var content:String
-     var subitems:[item]
+    var done:Bool
+    var content:String
+    var subitems:[item]
     init(newcontent:String, newdone:Bool, newsubitems:[item]) {
         done = newdone
         content = newcontent
