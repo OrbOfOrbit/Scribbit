b4fc350f03ae879c26c14b32e2a55190cce88f68
Alexander Romine <s013386@lm-20s013386.lmsd.org>
2019-02-22 11:47:34 -0500

    made it so you can call a list from the ui and set its title urself

--- Scribbit/editviewcontroller.swift
+++ Scribbit/editviewcontroller.swift
@@ -1,16 +1,10 @@
-//
-//  tableViewController.swift
-//  listedit
-//
-//  Created by Alexander Romine (student LM) on 2/3/19.
-//  Copyright © 2019 Alexander Romine (student LM). All rights reserved.
-//
-
 import UIKit
 
-class editViewController: UITableViewController  {
+class editViewController: UITableViewController {
+    
+    
     //@IBOutlet var tableview: UITableView!
-   
+    
     @IBOutlet var tableview: UITableView!
     let basicarray = [item(newcontent: "s", newdone: false, newsubitems: [item]()), item(newcontent: "b", newdone: false, newsubitems: [item]())]
     var basicitem : item = item(newcontent: "", newdone: true, newsubitems: [item]())
@@ -19,7 +13,7 @@ class editViewController: UITableViewController  {
         super.viewDidLoad()
         basicitem = item(newcontent: "i love fortnite and video games", newdone: false, newsubitems: basicarray)
         let x = [basicitem]
-         basiclist = list(newitems: x, newtitle: "basic list", newcreated: Date.init())
+        //basiclist = list(newitems: x, newtitle: "basic list", newcreated: Date.init())
         // Do any additional setup after loading the view.
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -31,6 +25,9 @@ class editViewController: UITableViewController  {
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
                 let textField = alert!.textFields![0]
                 self.basiclist.title = textField.text!
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
             }))
             self.present(alert, animated: true, completion: nil)
             DispatchQueue.main.async {
@@ -44,26 +41,46 @@ class editViewController: UITableViewController  {
             }
             alert.addAction(UIAlertAction(title: "add", style: .default, handler: { [weak alert] (_) in
                 self.basiclist.items.append(item(newcontent: (alert?.textFields![0].text)!, newdone: false, newsubitems: [item]()))
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
                 }
             ))
             self.present(alert, animated: true, completion: nil)
+            
+            
             
             DispatchQueue.main.async {
                 self.tableView.reloadData()
             }
         }
+            /*  else{
+             let alert = UIAlertController(title: "item", message: "", preferredStyle: .alert)
+             alert.addTextField { (textField) in
+             textField.text = self.basiclist.items[indexPath.row-1].content
+             }
+             alert.addAction(UIAlertAction(title: "done", style: .default, handler: { [weak alert] (_) in
+             let textField = alert!.textFields![0]
+             self.basiclist.items[indexPath.row-1].content = textField.text!
+             }))
+             
+             self.present(alert, animated: true, completion: nil)
+             tableView.reloadData()
+             }
+             DispatchQueue.main.async {
+             self.tableView.reloadData()
+             }*/
         else{
-            let alert = UIAlertController(title: "item", message: "", preferredStyle: .alert)
-            alert.addTextField { (textField) in
-                textField.text = self.basiclist.items[indexPath.row-1].content
+            let controller = itemViewController()
+            controller.itemtoedit = basiclist.items[indexPath.row-1]
+            self.present(controller, animated: true, completion: nil)
+            controller.callback = { result in
+                self.basiclist.items[indexPath.row-1] = result
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
             }
-            alert.addAction(UIAlertAction(title: "done", style: .default, handler: { [weak alert] (_) in
-                let textField = alert!.textFields![0]
-                self.basiclist.items[indexPath.row-1].content = textField.text!
-            }))
             
-            self.present(alert, animated: true, completion: nil)
-            tableView.reloadData()
         }
         DispatchQueue.main.async {
             self.tableView.reloadData()
@@ -84,11 +101,11 @@ class editViewController: UITableViewController  {
         else{
             cell?.textLabel?.text = basiclist.items[indexPath.row-1].content
             if basiclist.items[indexPath.row-1].done == true{
-                cell?.textLabel?.text?.append("[✓]")
+                cell?.textLabel?.text?.append("✅")
                 cell?.setNeedsDisplay()
             }
             else{
-                cell?.textLabel?.text?.append("[X]")
+                cell?.textLabel?.text?.append("❌")
                 cell?.setNeedsDisplay()
             }
         }
@@ -98,18 +115,16 @@ class editViewController: UITableViewController  {
     override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]?
     {
         // 1
-        //change "UITableViewRowActionStyle.default" to "UITableViewRowAction.Style.default"
         if indexPath.row != 0 && indexPath.row != basiclist.items.count+1{
-            let doneaction = UITableViewRowAction(style: UITableViewRowActionStyle.default, title: "done" , handler: { (action:UITableViewRowAction, indexPath: IndexPath) -> Void in
+            let doneaction = UITableViewRowAction(style: UITableViewRowAction.Style.default, title: "done" , handler: { (action:UITableViewRowAction, indexPath: IndexPath) -> Void in
                 self.basiclist.items[indexPath.row-1].done = true
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
-                    
                 }
                 
             })
             // 3
-            let notdoneaction = UITableViewRowAction(style: UITableViewRowActionStyle.default, title: "not done" , handler: { (action:UITableViewRowAction, indexPath:IndexPath) -> Void in
+            let notdoneaction = UITableViewRowAction(style: UITableViewRowAction.Style.default, title: "not done" , handler: { (action:UITableViewRowAction, indexPath:IndexPath) -> Void in
                 self.basiclist.items[indexPath.row-1].done = false
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
@@ -119,17 +134,16 @@ class editViewController: UITableViewController  {
             return [doneaction,notdoneaction]}
         return [UITableViewRowAction]()
     }
-   // override func table
+    // override func table
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
