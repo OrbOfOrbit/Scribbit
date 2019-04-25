655637a4ea27612cdb3ee63251559e8eceb1124f
Justin Cannan (student LM) <s016298@lm-20s016298.fios-router.home>
2019-04-25 16:26:46 -0400

    finished sharing list

---
+++ Scribbit/ShareListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ShareListViewController.swift
+//  Scribbit
+//
+//  Created by Justin Cannan (student LM) on 4/22/19.
+//  Copyright © 2019 Oron Barash (student LM). All rights reserved.
+//
+
+import UIKit
+
+class ShareListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Scribbit/user.swift
@@ -0,0 +1,9 @@
+//
+//  user.swift
+//  Scribbit
+//
+//  Created by Justin Cannan (student LM) on 4/24/19.
+//  Copyright © 2019 Oron Barash (student LM). All rights reserved.
+//
+
+import Foundation
